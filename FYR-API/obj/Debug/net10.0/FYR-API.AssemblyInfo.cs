5a65e877f232cb250fc896f197300270b74764c1
roci0milanese <rociomilanese28@gmail.com>
2026-05-15 20:35:50 -0300

    Fix gitignore

--- FYR-API/obj/Debug/net10.0/FYR-API.AssemblyInfo.cs
+++ FYR-API/obj/Debug/net10.0/FYR-API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FYR-API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce7ef350af20a9b6705e1fed8ada3f5ca2ac0aaa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9ae97586ad35d0b575800b731ccdd3cf24d0151")]
 [assembly: System.Reflection.AssemblyProductAttribute("FYR-API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FYR-API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
